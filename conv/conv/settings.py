d034321a292e6a9f2766eed1230633fb21fc27c5
posttsoi <deserza@gmail.com>
2020-03-09 13:15:54 +0700

    add psql make migrations

--- conv/conv/settings.py
+++ conv/conv/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'conv.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+                'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                'NAME': 'convert',
+                'USER': 'dorjctuser',
+                'PASSWORD': 'lexaprilip',
+                'HOST': 'localhost',
+                'PORT': '',
+                }
+            }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
