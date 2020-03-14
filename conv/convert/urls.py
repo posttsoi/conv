99a3780ee4facc3c752ee35cbe33ffa2faf5be66
posttsoi <deserza@gmail.com>
2020-03-14 20:45:54 +0700

    fixup! added <form>

--- conv/convert/urls.py
+++ conv/convert/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import home_page
+from .views import convert
 
 
 urlpatterns = [
-    path('', home_page)
+    path('', home_page),
+    path('convert/', convert, name='convert'),
 ]
--- conv/convert/views.py
+++ conv/convert/views.py
@@ -7,3 +7,8 @@ from .models import ExchangeRates
 def home_page(request):
     rates = ExchangeRates.objects.all()
     return render(request, 'convert/home_page.html', context={'rates': rates})
+
+
+def convert(request):
+    a = request.Post
+    return HttpResponse('<h1>{{a}</h1>')
